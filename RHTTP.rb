46f12c816c0dcf96d1302743095711c56610ee77
Mateusz Zając <zajac.mateusz@icloud.com>
2013-11-27 23:58:39 +0100

    Implementing the basic stuff of the server

--- RHTTP.rb
+++ RHTTP.rb
@@ -28,20 +28,45 @@ def content_type(path)
   CONTENT_TYPE.fetch(extension, DEFAULT_CONTENT_TYPE)
 end
 
+def requested_file(request_line)
+  
+end
+
 loop do
   Thread.start(server.accept) do |client|
     
     # Getting request-line and displaying it
     request = client.gets
     puts request
+  
+    # Getting the path of the resource server is asked for
+    path = requested_file(request)
     
-    response = "Hello!"
-    client.print "HTTP/1.1 200 OK\r\n" +
-                 "Content-Type: text/plain\r\n" +
-                 "Content-Length: #{response.bytesize}\r\n" +
-                 "Connection: close\r\n"
-    client.print "\r\n"
-    client.print response
+    # Checking if file exists and is not a directory
+    if File.exists?(path) && !File.directory?(path)
+      File.open(path, "rb") do |file| 
+        client.print "HTTP/1.1 200 OK\r\n" +
+                     "Content-Type: #{content_type(file)}\r\n" +
+                     "Content-Length: #{file.size}\r\n" +
+                     "Connection: close\r\n"
+        client.print "\r\n" 
+        
+        # Copying the content of file to the client (socket)
+        IO.copy_stream(file, client)
+      end
+    else
+      message = "File not found\n"
+      
+      # Giving 404 error if file wasn't found in root directory
+      client.print "HTTP/1.1 404 Not Found\r\n" +
+                   "Content-Type: text/plain\r\n" +
+                   "Content-Length: #{message.size}\r\n" +
+                   "Connection: close\r\n"
+      client.print "\r\n"
+      
+      client.print message            
+    end
+    # Closing the sessions with client (socket)
     client.close
   end
 end
